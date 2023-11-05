aa15a2eeecf3e0d4c42b9be7f12afcfba737c4ee
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2023-11-05 15:57:35 +0100

    modul6

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,9 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector(`ul#ingredients`);
+const getLiMarkup = (ingredient) => `<li class="item">${ingredient}</li>`
+const getListMarkup = (ingredientsList) => ingredientsList.map(it => getLiMarkup(it)).join('')
+
+ingredientsRef.insertAdjacentHTML('afterbegin', getListMarkup(ingredients));
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,21 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const ref = {
+  btn: document.querySelector('button.change-color'),
+  body: document.body,
+  colorSpan: document.querySelector('span.color'),
+}
+ref.btn.addEventListener('click', handleClickBtn)
+
+function handleClickBtn() {
+  const color = getRandomHexColor();
+  ref.body.style.backgroundColor = color;
+  ref.colorSpan.textContent = color;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
